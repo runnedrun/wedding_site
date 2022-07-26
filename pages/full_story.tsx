d6a8b5c951b1b034b97c6df043208658e9212e10
David Gaynor <runnedrun@gmail.com>
2022-07-26 11:35:35 +0200

    updated story page

--- pages/full_story.tsx
+++ pages/full_story.tsx
@@ -7,49 +7,54 @@ import { filtered } from "@/data/paramObsBuilders/filtered"
 import { map } from "rxjs"
 import { RsvpYes } from "@/data/types/RsvpYes"
 import { runWith } from "firebase-functions/v1"
+import Link from "next/link"
 
-const dataFn = () =>
-  combine({
-    allRsvps: filtered("rsvpYes").pipe(
-      map((_) =>
-        _.sort((a, b) => a.updatedAt?.toMillis() - b.updatedAt?.toMillis())
-      )
-    ),
-  })
-
-const storyStart =
-  "Once upon a time Xinqing and David were living happily in Nairobi, not knowing each other. They liked their lives but they felt like something was missing..."
-
-const StoryPage = component(dataFn, ({ allRsvps }) => {
-  const rows = [...allRsvps].sort(
-    (a, b) => a.updatedAt?.toMillis() - b.updatedAt?.toMillis()
-  )
-
-  rows.unshift({ storyAddition: storyStart } as RsvpYes)
-
+const BothStoriesPage = () => {
   return (
-    <div className="p-10">
-      <div className="mb-5 text-2xl">How Xinqing and David met</div>
-      {rows
-        .filter((_) => !_.hidden)
-        .map((row, i) => {
-          return (
-            <div className="mb-5" key={i}>
-              {row.names && (
-                <div className="mb-2 text-lg font-bold">
-                  {row.names.join(", ")}
-                </div>
-              )}
-              <div>{row.storyAddition}</div>
-            </div>
-          )
-        })}
+    <div className=" flex flex-col items-center p-10">
+      <div className=" max-w-xl">
+        <div className="mb-5 text-2xl">
+          Check out these videos explaining how David and Xinqing met and fell
+          in love:
+        </div>
+        <div className="mb-10">
+          <div className="mb-5 text-xl">
+            The version written by our wedding guests:
+          </div>
+          <a
+            className=" mb-5 block cursor-pointer rounded-md border-4 bg-blue-200 p-5"
+            href="https://youtu.be/e0Q0IX8rWzk"
+          >
+            View Video
+          </a>
+          <div>
+            <Link href={"/fake_story_script"}>
+              <div className=" cursor-pointer rounded-md border-4 bg-blue-200 p-5">
+                View Transcript
+              </div>
+            </Link>
+          </div>
+        </div>
+
+        <div>
+          <div className="text-xl">The "real" version:</div>
+          <div className=" font-bold">Coming soon!</div>
+          {/* <div>
+          <Link href={"/real_story_script"}>
+            <div>View Transcript</div>
+          </Link>
+        </div>
+        <div>
+          <Link href={"/fake_story_script"}>
+            <div>View Transcript</div>
+          </Link>
+        </div> */}
+        </div>
+      </div>
     </div>
   )
-})
+}
 
-StoryPage.displayName = "StoryPage"
+BothStoriesPage.displayName = "BothStoriesPage"
 
-export const getServerSideProps = buildPrefetchHandler(dataFn)
-
-export default StoryPage
+export default BothStoriesPage
